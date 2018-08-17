83adfc3b1d18a776407104873f59fa8d04e0351b
Lukasz Iwanecki <lukasz.iwanecki@pl.abb.com>
2018-08-17 11:15:49 +0200

    Roman converts correctly roman numbers form I - XXI. Loop version of the convertion

--- romanToArabic/main.cpp
+++ romanToArabic/main.cpp
@@ -28,28 +28,41 @@ public:
 
 	uint32_t toArabic() const {
 		int res = 0;
-		uint32_t curArabic = 0;
 		uint32_t prevArabic = 0;
 
 		for (auto it = begin(_roman); it != end(_roman); ++it) {
-			curArabic = _getArabic(toupper(*it));
-			if (curArabic > prevArabic) {
-				res = -res;
-			}
-			res += curArabic;
-			prevArabic = curArabic;
+			uint32_t value = _getArabic(*it);
+			res += _convertRomanSymbol(value, prevArabic);
 		}
+		res += _convertRomanSymbol(0, prevArabic);
 
 		return res;
+	}
+
+	int _convertRomanSymbol(uint32_t value, uint32_t & previousArabic) const {
+		int res;
+		if (value > previousArabic) res = _negative(previousArabic);
+		else res = _positive(previousArabic);
+		previousArabic = value;
+		return res;
+	}
+	
+	int _negative(uint32_t num) const {
+		return -_positive(num);
+	}
+
+	int _positive(uint32_t num) const {
+		return static_cast<int>(num);
 	}
 
 	uint32_t _getArabic(char roman) const {
 		using RomanArabicConvertItem = pair<char, uint32_t>;
 		using RomanArabicConvertVector = vector<RomanArabicConvertItem>;
 		const RomanArabicConvertVector ROMAN_ARABIC_CONVERT_VECTOR{
-			{ 'I', 1 },{ 'V', 5 }//, {'X', 10}, {'L', 50}, {'C', 100}, {'M', 1000}
+			{ 'I', 1 },{ 'V', 5 }, {'X', 10}, {'L', 50}, {'C', 100}, {'M', 1000}
 		};
 
+		roman = toupper(roman);
 		auto it = find_if(
 			begin(ROMAN_ARABIC_CONVERT_VECTOR),
 			end(ROMAN_ARABIC_CONVERT_VECTOR),
@@ -71,17 +84,10 @@ private:
 	string _roman;
 };
 
-TEST(ARoman, GeneratesArabic1WhenRomanIIsGiven) {
-	ASSERT_THAT(Roman("I").toArabic(), Eq(1));
-}
-
 TEST(ARoman, Generates0WhenEmptyStringIsGiven) {
 	ASSERT_THAT(Roman("").toArabic(), Eq(0));
 }
 
-TEST(ARoman, GeneratesArabic5WhenRomanVIsGiven) {
-	ASSERT_THAT(Roman("V").toArabic(), Eq(5));
-}
 
 TEST(ARoman, ThrowsWhenIllegalSymbolDetected) {
 	ASSERT_THROW(Roman("a").toArabic(), Roman::IllegalSymbol);
@@ -92,11 +98,7 @@ TEST(ARoman, GeneratesTheSameOutputForInputInUpperAndLowerCase) {
 	EXPECT_THAT(Roman("I").toArabic(), Roman("i").toArabic());
 }
 
-TEST(ARoman, GeneratesArabic4WhenRomanIVIsGiven) {
-	ASSERT_THAT(Roman("IV").toArabic(), Eq(4));
-}
-
-TEST(ARoman, GeneratesCorrectArabicForRomanFromItoVIII) {
+TEST(ARoman, GeneratesCorrectArabicFromRoman) {
 	EXPECT_THAT(Roman("I").toArabic(), Eq(1));
 	EXPECT_THAT(Roman("II").toArabic(), Eq(2));
 	EXPECT_THAT(Roman("III").toArabic(), Eq(3));
@@ -105,4 +107,17 @@ TEST(ARoman, GeneratesCorrectArabicForRomanFromItoVIII) {
 	EXPECT_THAT(Roman("VI").toArabic(), Eq(6));
 	EXPECT_THAT(Roman("VII").toArabic(), Eq(7));
 	EXPECT_THAT(Roman("VIII").toArabic(), Eq(8));
+	EXPECT_THAT(Roman("IX").toArabic(), Eq(9));
+	EXPECT_THAT(Roman("X").toArabic(), Eq(10));
+	EXPECT_THAT(Roman("XI").toArabic(), Eq(11));
+	EXPECT_THAT(Roman("XII").toArabic(), Eq(12));
+	EXPECT_THAT(Roman("XIII").toArabic(), Eq(13));
+	EXPECT_THAT(Roman("XIV").toArabic(), Eq(14));
+	EXPECT_THAT(Roman("XV").toArabic(), Eq(15));
+	EXPECT_THAT(Roman("XVI").toArabic(), Eq(16));
+	EXPECT_THAT(Roman("XVII").toArabic(), Eq(17));
+	EXPECT_THAT(Roman("XVIII").toArabic(), Eq(18));
+	EXPECT_THAT(Roman("XIX").toArabic(), Eq(19));
+	EXPECT_THAT(Roman("XX").toArabic(), Eq(20));
+	EXPECT_THAT(Roman("XXI").toArabic(), Eq(21));
 }
